42b507c3167fbc4ac18efadd1d50083a60dd9ed9
maestro19 <sreeramdgr8@gmail.com>
2015-04-30 03:12:48 +0530

    added header
    with logo and heading

--- AboutUs.java
+++ AboutUs.java
@@ -1,54 +1,67 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.*;
 
-class AboutUs
+
+
+
+public class AboutUs
 {
-	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	
 	AboutUs()
 	{
-		
-		JFrame aboutus = new JFrame("About Us");
-		// aboutus.setFont("TimesNewRoman",Font.BOLD,50);
-		/* aboutus.setLayout(null); */
-		aboutus.setLayout(new FlowLayout());
-		aboutus.setSize(1000,1000);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
 		
+		//created aboutusframe frame
+		JFrame aboutusframe = new JFrame("About Us");
+		aboutusframe.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		aboutusframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		aboutusframe.setVisible(true);
+		aboutusframe.setLayout(null);
+		//int width = aboutusframe.getSize().width;
+		
+		//created headerpanel
+		JPanel headerpanel = new JPanel();
+		headerpanel.setLayout(null);
+		headerpanel.setBounds(5,5,screenSize.width-10,100);
+		headerpanel.setBackground(Color.cyan);
+		
+		
+		JLabel heading = new JLabel("XXX Hospital");
+		heading.setFont(new Font("TimesNewRoman",Font.BOLD,40));
+		heading.setBackground(Color.red);
+		heading.setBounds(screenSize.width-550,30,500,50);
+		
+		JButton logo = new JButton();
+		logo.setBackground(Color.black);
+		ImageIcon icon = new ImageIcon("C:\\Users\\Sreeram\\Desktop\\Project\\Images\\logo.png");
+		Image img = icon.getImage();
+		Image newimg = img.getScaledInstance(100, 100, Image.SCALE_SMOOTH ) ;  
+		ImageIcon logoicon = new ImageIcon(newimg);
+		logo.setIcon(logoicon);
+		logo.setBounds(50,0,100,100);
+		
+		
+		aboutusframe.add(headerpanel);
+		headerpanel.add(heading);
+		headerpanel.add(logo);
 		
 		
 		
-		aboutus.getContentPane().setBackground(Color.BLACK);
-		// aboutus.setLayout(new BorderLayout());
-		
-		// aboutus.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		aboutus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// JLabel jlab1 = new JLabel("TEXT");
-		// aboutus.add(jlab1);
 		
 		
-		// aboutus.setUndecorated(true);
-		 // aboutus.setOpacity(0.5f);
-		JLabel jlab = new JLabel(new ImageIcon("aboutus.jpg"));
-		jlab.setBounds(0,0,1000,1000);
-		// jlab.setOpacity(0.5f);
-		aboutus.setContentPane(jlab);
-		aboutus.setVisible(true);
+		
 	}
-	
 	
 	public static void main(String a[])
 	{
-		// SwingUtilities.invokeLater(new Runnable()
-		// {
-			// public void run()
-			// {
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
 				new AboutUs();
-			// }
-		// });
+			}
+		});
 	}
-
 }
