fb4db7bf29c699fb7061939a6369389a1e248075
diabolicfreak <diabolicfreak@gmail.com>
2015-04-29 15:35:19 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,25 +1,49 @@
-import.java.awt.*;
-import.java.awt.events.*;
-import.java.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 class HomePage
 {	
-	JButton about, contact; 
+	JButton about, contact;
+	
 
 	HomePage()
 	{
-		Jframe homepageframe = new JFrame("Welcome to XXX Hospital");
+		JFrame homepageframe = new JFrame("Welcome to XXX Hospital");
 		
-		homepageframe.setLayout(null);
+		homepageframe.setLayout(new GridLayout());
+		
+		homepageframe.setSize(700,700);
 		
 		homepageframe.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		
 		homepageframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		about = new JButton("About Us");
-		contact = new JButton("Contact Us");
+		contact = new JButton("Contact Us");		
 		
-		about.addActionListener (new ActionListener())
+		about.setSize(20,20);
+		contact.setSize(40,40);
 		
-	}	
+		homepageframe.add(about);
+		homepageframe.add(contact);
+		
+		homepageframe.add(test);
+		
+		about.addActionListener (new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				homepageframe.setVisible(true);
+			}
+		});
+		
+		homepageframe.setVisible(true);
+		
+	}
+
+	public static void main(String ar[])
+	{
+		new HomePage(); 
+	}
 }
