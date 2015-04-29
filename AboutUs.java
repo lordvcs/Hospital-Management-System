c9690009524649a4ebd737fd15133d9084d95063
maestro19 <sreeramdgr8@gmail.com>
2015-04-29 14:36:57 +0530

    Update AboutUs.java

--- AboutUs.java
+++ AboutUs.java
@@ -3,30 +3,52 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-public class AboutUs
+class AboutUs
 {
+	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+	
 	AboutUs()
 	{
-		JFrame aboutus = new JFrame("About Us");
-		aboutus.setLayout(null);
-		aboutus.setSize(500,500);
-		aboutus.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		aboutus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		aboutus.setVisible(true);
-		aboutus.setLayout(new BorderLayout());
-		aboutus.setContentPane(new JLabel(new ImageIcon("aboutus_image.jpg")));
-		aboutus.setLayout(new FlowLayout());
 		
+		JFrame aboutus = new JFrame("About Us");
+		// aboutus.setFont("TimesNewRoman",Font.BOLD,50);
+		/* aboutus.setLayout(null); */
+		aboutus.setLayout(new FlowLayout());
+		aboutus.setSize(1000,1000);
+		
+		
+		
+		
+		
+		aboutus.getContentPane().setBackground(Color.BLACK);
+		// aboutus.setLayout(new BorderLayout());
+		
+		// aboutus.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		aboutus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// JLabel jlab1 = new JLabel("TEXT");
+		// aboutus.add(jlab1);
+		
+		
+		// aboutus.setUndecorated(true);
+		 // aboutus.setOpacity(0.5f);
+		JLabel jlab = new JLabel(new ImageIcon("aboutus.jpg"));
+		jlab.setBounds(0,0,1000,1000);
+		// jlab.setOpacity(0.5f);
+		aboutus.setContentPane(jlab);
+		aboutus.setVisible(true);
 	}
+	
 	
 	public static void main(String a[])
 	{
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			public void run()
-			{
+		// SwingUtilities.invokeLater(new Runnable()
+		// {
+			// public void run()
+			// {
 				new AboutUs();
-			}
-		});
+			// }
+		// });
 	}
+
 }
