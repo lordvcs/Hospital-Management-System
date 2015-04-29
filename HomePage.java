856aef82bb3a6a897964941b6c40872cbce82db2
diabolicfreak <diabolicfreak@gmail.com>
2015-04-29 08:56:21 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,4 +1,25 @@
-public class HomePage 
-{
-	
+import.java.awt.*;
+import.java.awt.events.*;
+import.java.swing.*;
+
+class HomePage
+{	
+	JButton about, contact; 
+
+	HomePage()
+	{
+		Jframe homepageframe = new JFrame("Welcome to XXX Hospital");
+		
+		homepageframe.setLayout(null);
+		
+		homepageframe.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		
+		homepageframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		about = new JButton("About Us");
+		contact = new JButton("Contact Us");
+		
+		about.addActionListener (new ActionListener())
+		
+	}	
 }
