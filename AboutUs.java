48249d3b4ec1a9a8cdf8389c30596a047766e22a
maestro19 <sreeramdgr8@gmail.com>
2015-05-02 01:01:05 +0530

    About Us mod 2

--- AboutUs.java
+++ AboutUs.java
@@ -37,7 +37,7 @@ public class AboutUs
 		//create logo button JButton
 		JButton logo = new JButton();
 		logo.setBackground(Color.black);
-		ImageIcon icon = new ImageIcon("C:\\Users\\Sreeram\\hms\\Images\\logo.png");
+		ImageIcon icon = new ImageIcon("Images//logo.png");
 		Image img = icon.getImage();
 		Image newimg = img.getScaledInstance(100, 100, Image.SCALE_SMOOTH ) ;  
 		ImageIcon logoicon = new ImageIcon(newimg);
@@ -45,10 +45,22 @@ public class AboutUs
 		logo.setBounds(50,0,100,100);
 		
 		
+		//create mainbodypanel JPanel
+		JPanel mainbodypanel = new JPanel();
+		mainbodypanel.setLayout(null);
+		mainbodypanel.setBounds(5,110,screenSize.width-10,screenSize.height-(screenSize.height/4));
+		mainbodypanel.setBackground(Color.black);
+		
+		//create mainbodypanel background image
+		JLabel 
+		
+		//add headerpanel
 		aboutusframe.add(headerpanel);
 		headerpanel.add(heading);
 		headerpanel.add(logo);
 		
+		//add mainbodypanel
+		aboutusframe.add(mainbodypanel);
 		
 		
 		
